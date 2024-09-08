78effe8db21b6737e2e11a708f254d87afbd32bd
Munira12345 <munirangomo@gmail.com>
2024-09-08 16:04:27 +0300

    might have to remove firebase analytics then try the api first

--- firebaseDependencies/src/main/java/firebaseModule.kt
+++ firebaseDependencies/src/main/java/firebaseModule.kt
@@ -2,16 +2,13 @@ package com.munirasapplication.firebaseDependencies
 
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
+import android.app.Application
+import org.koin.core.module.Module
 import org.koin.dsl.module
-import com.munirasapplication.app.appcomponents.di.MyApp
+// still having an issue with firebase analytics might have to remove then try the api first
 
-/*
-fun firebaseModule(): Module {
-    val firebaseModule = module {
-        single<Firebase> { Firebase.initialize(this@MyApp) }
-    }
-    return firebaseModule
-}*/
+/*run app with module changes
+* */
 fun firebaseModule(): Module {
     val firebaseModule = module {
         single<Firebase> { Firebase.initialize(applicationContext) } // Use applicationContext
