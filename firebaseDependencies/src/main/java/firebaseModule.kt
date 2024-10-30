271b7bb6cc28f1460ec4c9bc5bc57188ba94f68b
Munira12345 <munirangomo@gmail.com>
2024-10-30 19:05:30 +0300

    build issue

--- firebaseDependencies/src/main/java/firebaseModule.kt
+++ firebaseDependencies/src/main/java/firebaseModule.kt
@@ -3,6 +3,11 @@ package com.munirasapplication.firebaseDependencies
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 // still having an issue with firebase analytics might have to remove then try the api first
@@ -10,12 +15,16 @@ import org.koin.dsl.module
 
 /*run app with module changes
 * */
-
 fun firebaseModule(): Module {
+    // Define the Koin module
+    return module {
+        // Initialize FirebaseApp using androidContext from Koin
+        single<FirebaseApp> { FirebaseApp.initializeApp(androidContext())!! } // Use androidContext() from Koin
 
-    val firebaseModule = module {
-        single<Firebase> { Firebase.initialize(applicationContext) } // Use applicationContext
+        // Inject specific Firebase services
+        single { FirebaseAuth.getInstance() }
+        single { FirebaseFirestore.getInstance() }
+        single { FirebaseAnalytics.getInstance(get()) }
     }
-    return firebaseModule
 }
 // compile 'com.facebook.android:facebook-share:latest.release' add in build.gradle dependencies
